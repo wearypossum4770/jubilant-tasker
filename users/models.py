a255860c1ad3b901a4c1a3f3480e2c42e9cf189e
Stephen Smith <wearypossum4770@yahoo.com>
2021-04-17 11:55:48 -0400

    sync: <chromebook>

--- users/models.py
+++ users/models.py
@@ -1,29 +1,44 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db.models import (
-  CharField,
-   OneToOneField, BooleanField, Model, PositiveSmallIntegerField,
-   ManyToManyField,DateField,
-   IntegerChoices,  
-   SET_NULL, ImageField,DateTimeField,
+    SET_NULL,
+    BooleanField,
+    CharField,
+    DateField,
+    DateTimeField,
+    ImageField,
+    IntegerChoices,
+    ManyToManyField,
+    Model,
+    OneToOneField,
+    PositiveSmallIntegerField,
 )
 from django.utils.translation import gettext_lazy as _
+
+
 def user_directory_path(instance, filename):
     return f"user_{instance.user.id}/profile_pics/{filename}"
+
+
 class Role(Model):
     class UserType(IntegerChoices):
-        REGULAR  = 0, _("Regular User")
-        STUDENT = 1 , _("Student")
+        REGULAR = 0, _("Regular User")
+        STUDENT = 1, _("Student")
         TEACHER = 2, _("Teacher")
         SECRETARY = 3, _("Secretary")
         SUPERVISOR = 4, _("Supervisor")
         ADMIN = 5, _("Administrator")
-    user_type = PositiveSmallIntegerField(choices=UserType.choices,default=UserType.REGULAR, primary_key=True)
+
+    user_type = PositiveSmallIntegerField(
+        choices=UserType.choices, default=UserType.REGULAR, primary_key=True
+    )
+
 
 class User(AbstractUser):
     roles = ManyToManyField(Role)
     middle_name = CharField(max_length=30, blank=True)
     date_of_birth = DateField(null=True, blank=True)
+
 
 class Profile(Model):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
@@ -33,4 +48,6 @@ class Profile(Model):
     is_public = BooleanField(default=False)
     is_active = BooleanField(default=True)
     image = ImageField(default="default.webp", upload_to=user_directory_path)
+
+
 #  have rust task using celery or golang task to convert image files to webp.
